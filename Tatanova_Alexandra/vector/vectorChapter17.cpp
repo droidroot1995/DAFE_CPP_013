6d35caf1f67cddf78fc585159b5a67fd5a5e7f6f
alexa <aleksandratatanova3830@outlook.com>
2020-12-11 01:03:49 +0300

    "

--- Tatanova_Alexandra/vector/vectorChapter17.cpp
+++
@@ -1,8 +0,0 @@
-#include "vectorChapter17.h"
-
-vector:: vector (int s) // Конструктор
-    : sz{s},  elem { new double[s]} // Выделение памяти
-{
-    for (int i = 0; i<s; ++i)
-        elem[i] = 0;
-}
